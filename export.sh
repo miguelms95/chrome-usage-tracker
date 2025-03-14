fc5bcea033b30940e6c0e1b93eef4694465412d4
Miguel Martínez Serrano <mmartinez@nextdigital.es>
2025-03-14 18:06:44 +0100

    feat: update styles, add export command and remove unused assets

---
+++ export.sh
@@ -0,0 +1,2 @@
+
+zip chromeusagetracker.zip $(find . -not -path "./docs*" -not -path "./.git*" -not -path "./.idea*" -not -path "./local*" -not -name "Makefile" -not -name "README.md" -not -name ".DS_Store" -not -name "chromeusagetracker.zip")
