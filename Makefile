aeb3357b3c004ad0e03a8a385c30cb15db4999d0
Miguel Martínez Serrano <mmartinez@nextdigital.es>
2025-03-18 17:04:43 +0100

    release: 1.1

--- Makefile
+++ Makefile
@@ -1,3 +1,4 @@
 
 zip:
-	zip chrome\-usage\-tracker.zip $(find . -not -path "./docs*" -not -path "./.git*" -not -path "./.idea*" -not -path "./local*" -not -name "Makefile" -not -name "README.md" -not -name ".DS_Store" -not -name "chrome\-usage\-tracker.zip")
+	@echo "run export.sh 🗃️"
+	@./export.sh
--- export.sh
+++ export.sh
@@ -1,2 +1,2 @@
 
-zip chromeusagetracker.zip $(find . -not -path "./docs*" -not -path "./.git*" -not -path "./.idea*" -not -path "./local*" -not -name "Makefile" -not -name "README.md" -not -name ".DS_Store" -not -name "chromeusagetracker.zip")
+zip chrome-usage-tracker.zip $(find . -not -path "./docs*" -not -path "./.git*" -not -path "./.idea*" -not -path "./local*" -not -name "Makefile" -not -name "README.md" -not -name ".DS_Store" -not -name "chrome-usage-tracker.zip")
